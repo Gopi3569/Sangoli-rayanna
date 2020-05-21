c8fdca11a420a9747af44f6e81606fe7a6f6bf92
Gopi3569 <65659691+Gopi3569@users.noreply.github.com>
2020-05-21 18:16:02 +0530

    Update settings.py

--- Ownproject/Ownproject/settings.py
+++ Ownproject/Ownproject/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'Ownproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'DB108',
-        'USER':'postgres',
-        'PASSWORD':'gopi1997',
-        'HOST':'localhost',
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),        
     }
 }
 
